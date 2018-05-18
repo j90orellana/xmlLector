d10566807e0e8c0518142177d5b1c906b41d812b
j90orellana <j90orellana@hotmail.com>
2018-05-18 17:07:43 -0500

    Agregar archivos de proyecto.

---
+++ xml/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace xml
+{
+    partial class frmxml
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtruta = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnselec = new System.Windows.Forms.Button();
+            this.dtgconten = new System.Windows.Forms.DataGridView();
+            this.btncargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgconten)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ruta Xml";
+            // 
+            // txtruta
+            // 
+            this.txtruta.Location = new System.Drawing.Point(79, 20);
+            this.txtruta.Name = "txtruta";
+            this.txtruta.Size = new System.Drawing.Size(492, 22);
+            this.txtruta.TabIndex = 1;
+            this.txtruta.Text = "C:\\Users\\Hp\\Desktop\\MONEDA PAISES.xml";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnselec
+            // 
+            this.btnselec.Location = new System.Drawing.Point(577, 20);
+            this.btnselec.Name = "btnselec";
+            this.btnselec.Size = new System.Drawing.Size(75, 23);
+            this.btnselec.TabIndex = 2;
+            this.btnselec.Text = "Seleccionar";
+            this.btnselec.UseVisualStyleBackColor = true;
+            this.btnselec.Click += new System.EventHandler(this.btnselec_Click);
+            // 
+            // dtgconten
+            // 
+            this.dtgconten.AllowUserToResizeColumns = false;
+            this.dtgconten.AllowUserToResizeRows = false;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dtgconten.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            this.dtgconten.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgconten.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgconten.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dtgconten.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtgconten.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dtgconten.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.LightBlue;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(10)))), ((int)(((byte)(56)))));
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgconten.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgconten.DefaultCellStyle = dataGridViewCellStyle9;
+            this.dtgconten.EnableHeadersVisualStyles = false;
+            this.dtgconten.Location = new System.Drawing.Point(12, 54);
+            this.dtgconten.Name = "dtgconten";
+            this.dtgconten.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dtgconten.RowHeadersVisible = false;
+            this.dtgconten.RowTemplate.Height = 18;
+            this.dtgconten.Size = new System.Drawing.Size(721, 479);
+            this.dtgconten.TabIndex = 3;
+            // 
+            // btncargar
+            // 
+            this.btncargar.Location = new System.Drawing.Point(658, 20);
+            this.btncargar.Name = "btncargar";
+            this.btncargar.Size = new System.Drawing.Size(75, 23);
+            this.btncargar.TabIndex = 4;
+            this.btncargar.Text = "Cargar";
+            this.btncargar.UseVisualStyleBackColor = true;
+            this.btncargar.Click += new System.EventHandler(this.btncargar_Click);
+            // 
+            // frmxml
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 545);
+            this.Controls.Add(this.btncargar);
+            this.Controls.Add(this.dtgconten);
+            this.Controls.Add(this.btnselec);
+            this.Controls.Add(this.txtruta);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmxml";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XML";
+            this.Load += new System.EventHandler(this.frmxml_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgconten)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtruta;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnselec;
+        private System.Windows.Forms.DataGridView dtgconten;
+        private System.Windows.Forms.Button btncargar;
+    }
+}
+
---
+++ xml/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace xml
+{
+    public partial class frmxml : Form
+    {
+        public frmxml()
+        {
+            InitializeComponent();
+        }
+
+        private void btnselec_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Archivos xml|*.xml";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtruta.Text = openFileDialog1.FileName;
+            }
+        }
+        private void btncargar_Click(object sender, EventArgs e)
+        {
+            DataTable tablita = new DataTable();
+            tablita.Columns.Add("Denominación Moneda");
+            tablita.Columns.Add("Nombre Moneda");
+            XmlDocument listado = new XmlDocument();
+            listado.Load(txtruta.Text);
+
+            XmlNodeList elementos = listado.SelectNodes("moneda");
+            foreach (XmlNode item in listado.ChildNodes)
+            {
+                foreach (XmlNode items in item)
+                {
+                    DataRow filita = tablita.NewRow();
+                    if (items.Name != "moneda")
+                    {
+                        filita["Denominación Moneda"] = items.Attributes["value"].Value;
+                        filita["Nombre Moneda"] = items.InnerText;
+                        tablita.Rows.Add(filita);
+                    }
+                }
+            }
+            dtgconten.DataSource = tablita;
+        }
+
+        private void frmxml_Load(object sender, EventArgs e)
+        {
+            txtruta.Text = Application.StartupPath + @"\Xmls\MONEDA PAISES.xml";
+        }
+    }
+}
